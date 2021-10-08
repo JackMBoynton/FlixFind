219271541de28923c0b7c865526a558651a7d2c7
Jack Boynton <boyntonj.dev@gmail.com>
2021-10-08 17:52:17 +0100

    created MovieList component and started using OMDb API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+import MovieList from './components/MovieList';
 
 function App() {
+  const [movies, setMovies] = useState([
+    
+  ]);
+
+  const getMovieRequest = async () => {
+    const url = 'http://www.omdbapi.com/?s=The Godfather&apikey=2212bf28'
+
+    // sending a request using the above url and converting it to JSON
+    const response = await fetch(url);
+    const responseJSON = await response.json();
+
+    setMovies(responseJSON.Search);
+
+  }
+
+  // use the function for getting a response, when an array is empty - i.e. when page loads
+  useEffect(() => {
+    getMovieRequest();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='container-fluid movie-app'>
+        <div className='row'>
+          <MovieList movies={movies} />
+        </div>
+      </div>
   );
 }
 
 export default App;
+
