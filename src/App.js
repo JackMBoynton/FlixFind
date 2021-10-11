7db8a06d59f2f606a8b1158013c3fa4ee238d0a0
Jack Boynton <boyntonj.dev@gmail.com>
2021-10-12 00:49:10 +0100

    Removed a redundant line about Modal

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import MovieListHeading from "./components/MovieListHeading";
 import SearchBox from "./components/SearchBox";
 import AddFavourites from "./components/AddFavourites";
 import RemoveFavourites from "./components/RemoveFavourites";
-import MovieDataModal from "./components/MovieDataModal";
 
 function App() {
   // States
