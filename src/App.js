8df67e9d61b3e89bf90e17f44b3fb431bfcad8e8
Jack Boynton <boyntonj.dev@gmail.com>
2021-10-11 16:29:06 +0100

    Added effects for hovering movies, added in basic favouriting functionality - but not persistent

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-import MovieList from './components/MovieList';
-import MovieListHeading from './components/MovieListHeading';
-import SearchBox from './components/SearchBox';
+import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
+import AddFavourites from "./components/AddFavourites";
+import RemoveFavourites from "./components/RemoveFavourites";
 
 function App() {
+  // States
   const [movies, setMovies] = useState([]);
-  const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState("");
+  const [favourites, setFavourites] = useState([]);
 
   const getMovieRequest = async (searchValue) => {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=${process.env.REACT_APP_API_KEY}` // template string
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=${process.env.REACT_APP_API_KEY}`; // template string
 
     // sending a request using the above url and converting it to JSON
     const response = await fetch(url);
@@ -20,25 +24,55 @@ function App() {
     if (responseJSON.Search) {
       setMovies(responseJSON.Search);
     }
-  }
+  };
 
   // any value we add to the array - when it is updated; so the method will be run
   useEffect(() => {
     getMovieRequest(searchValue);
   }, [searchValue]);
 
+  const addFavouriteMovie = (movie) => {
+    const newFavouriteList = [...favourites, movie];
+    setFavourites(newFavouriteList);
+  };
+
+  const removeFavouriteMovie = (movie) => {
+    const newFavouriteList = favourites.filter(
+      (favourite) => favourite.imdbID !== movie.imdbID
+    );
+    setFavourites(newFavouriteList);
+  };
+
   return (
-      <div className='container-fluid movie-app'>
-        <div className="row d-flex align-items-center mt-4 mb-4">
-          <MovieListHeading heading='FlixFind'/>
-          <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
-        </div>
-        <div className='row'>
-          <MovieList movies={movies} />
-        </div>
+    <div className="container-fluid movie-app">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="FlixFind" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
       </div>
+      <div className="row">
+        {/* We need to pass the favourites component in as a prop, this means we can use different components for
+        different things */}
+        <MovieList
+          movies={movies}
+          handleFavouritesClick={addFavouriteMovie}
+          favouriteComponent={AddFavourites}
+        />
+      </div>
+
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Favourites" />
+      </div>
+      <div className="row">
+        {/* We need to pass the favourites component in as a prop, this means we can use different components for
+        different things */}
+        <MovieList
+          movies={favourites}
+          handleFavouritesClick={removeFavouriteMovie}
+          favouriteComponent={RemoveFavourites}
+        />
+      </div>
+    </div>
   );
 }
 
 export default App;
-
